b3df9c6625a32f26c16a080158ad99f9930567ce
Yuriy Pelekh <Yuriy.Pelekh@hotmail.com>
2013-07-25 22:20:18 +0300

    Improved UX: added mouse enter/leave events handling for points.

--- AudioVisualizer/AudioProcessorHelper.cs
+++ AudioVisualizer/AudioProcessorHelper.cs
@@ -4,66 +4,65 @@ using AudioProcessor;
 
 namespace AudioVisualizer
 {
-  public class AudioProcessorHelper
-  {
-    private readonly string waterMark;
-    private readonly Mode mode;
-
-    public AudioProcessorHelper(string waterMark, Mode mode)
+    public class AudioProcessorHelper
     {
-      this.waterMark = waterMark;
-      this.mode = mode;
-    }
+        private readonly string _waterMark;
+        private readonly Mode _mode;
 
-    public string Process(byte[] originalBytes, out byte[] markedBytesArray, out byte[] unWaterMarkedBytesArray, out int[] markedIndexes, out int[] unMarkedIndexes)
-    {
-      var processor = new Processor(originalBytes, mode);
-
-      markedBytesArray = processor.GetWaterMarkedBytes(waterMark);
-      unWaterMarkedBytesArray = processor.ExtractWaterMark(markedBytesArray);
-
-      markedIndexes = processor.InseredWaterMarkIndexes.ToArray();
-      unMarkedIndexes = processor.ExtractedWaterMarkIndexes.ToArray();
-
-      var percentage = CalculateResult(processor.OriginalWaterMarkBits, processor.ExtractedWaterMarkBits);
-      var percentageByIndexes = ResultByIndexes(markedIndexes, unMarkedIndexes);
-      var extractedWaterMark = WaterMark.FromBitArray(processor.ExtractedWaterMarkBits);
-      return string.Format("Identity: {0}% / {1}% -> {2}", percentage.ToString("F0"), percentageByIndexes.ToString("F0"), extractedWaterMark);
-    }
-
-    private double CalculateResult(byte[] originalWaterMarkBits, byte[] extractedWaterMarkBits)
-    {
-      var assertBitsCount = extractedWaterMarkBits.Length;
-      var persentage = 0.0;
-
-      for (var i = 0; i < assertBitsCount; i++)
-      {
-        if (extractedWaterMarkBits[i%extractedWaterMarkBits.Length] ==
-            originalWaterMarkBits[i%originalWaterMarkBits.Length])
+        public AudioProcessorHelper(string waterMark, Mode mode)
         {
-          persentage++;
+            _waterMark = waterMark;
+            _mode = mode;
         }
-      }
 
-      persentage /= assertBitsCount;
-
-      return persentage*100;
-    }
-
-    private double ResultByIndexes(int[] markedIndexes, int[] unMarkedIndexes)
-    {
-      var count = Math.Min(markedIndexes.Length, unMarkedIndexes.Length);
-      var result = 0.0;
-      for (int i = 0; i < count; i++)
-      {
-        if (markedIndexes[i] == unMarkedIndexes[i])
+        public string Process(byte[] originalBytes, out byte[] markedBytesArray, out byte[] unWaterMarkedBytesArray, out int[] markedIndexes, out int[] unMarkedIndexes)
         {
-          result++;
+            var processor = new Processor(originalBytes, _mode);
+
+            markedBytesArray = processor.GetWaterMarkedBytes(_waterMark);
+            unWaterMarkedBytesArray = processor.ExtractWaterMark(markedBytesArray);
+
+            markedIndexes = processor.InseredWaterMarkIndexes.ToArray();
+            unMarkedIndexes = processor.ExtractedWaterMarkIndexes.ToArray();
+
+            var percentage = CalculateResult(processor.OriginalWaterMarkBits, processor.ExtractedWaterMarkBits);
+            var percentageByIndexes = ResultByIndexes(markedIndexes, unMarkedIndexes);
+            var extractedWaterMark = WaterMark.FromBitArray(processor.ExtractedWaterMarkBits);
+            return string.Format("Identity: {0}% / {1}% -> {2}", percentage.ToString("F0"), percentageByIndexes.ToString("F0"), extractedWaterMark);
         }
-      }
-      result /= count;
-      result *= 100;
-      return result;
+
+        private static double CalculateResult(IList<byte> originalWaterMarkBits, IList<byte> extractedWaterMarkBits)
+        {
+            var assertBitsCount = extractedWaterMarkBits.Count;
+            var persentage = 0.0;
+
+            for (var i = 0; i < assertBitsCount; i++)
+            {
+                if (extractedWaterMarkBits[i%extractedWaterMarkBits.Count] == originalWaterMarkBits[i%originalWaterMarkBits.Count])
+                {
+                    persentage++;
+                }
+            }
+
+            persentage /= assertBitsCount;
+
+            return persentage*100;
+        }
+
+        private static double ResultByIndexes(IList<int> markedIndexes, IList<int> unMarkedIndexes)
+        {
+            var count = Math.Min(markedIndexes.Count, unMarkedIndexes.Count);
+            var result = 0.0;
+            for (var i = 0; i < count; i++)
+            {
+                if (markedIndexes[i] == unMarkedIndexes[i])
+                {
+                    result++;
+                }
+            }
+            result /= count;
+            result *= 100;
+            return result;
+        }
     }
-  }
 }
